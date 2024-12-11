30751ef7c4a18b6f4c2ed66f26848477de7d6311
TharunGowda-S <tharunkumartharun188@gmail.com>
2024-12-11 20:35:44 +0530

    Array Destructuring

---
+++ Section-03-Modern-Operators-and-Strings/Script.js
@@ -0,0 +1,9 @@
+//Destructuring Arrays
+// Array Destructuring means Unpack the values are stored in the Array in to a single variable.
+//ex:
+let fruits = ["apple", "ki-wi", "mango", "chickoo", "buterfruit"];
+console.log(fruits);
+let [f1, f2, f3, f4, f5] = fruits;
+console.log(f1, f2, f3, f4, f5);
+let [fruit1] = fruits;
+console.log(fruit1);
