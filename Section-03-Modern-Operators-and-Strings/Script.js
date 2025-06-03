bfa61d11fc339ea403506af2d40bfdd5db554114
Tharunkumar S <73631066+TharunGowda-S@users.noreply.github.com>
2025-06-03 22:39:29 +0530

    learn also about object destructuring and default vales and mutating variables

--- Section-03-Modern-Operators-and-Strings/Script.js
+++ Section-03-Modern-Operators-and-Strings/Script.js
@@ -7,3 +7,17 @@ let [f1, f2, f3, f4, f5] = fruits;
 console.log(f1, f2, f3, f4, f5);
 let [fruit1] = fruits;
 console.log(fruit1);
+
+// using number as a variable is not valid
+//let [1,2,3]=["a","b","c"];
+//console.log(1,2,3);
+
+console.log("==============================");
+
+// key points of Array destructuring
+// order matters: the order of variables in the LHS is corresponds to the values in  the given array.
+// skipping elements: we can also skip the elements by using a comma and leaving the space in the destructuring patterns.
+// ex:
+const [b, , c, d] = ["apple", "mango", "chickoo", "muskmelon"];
+// here the element mango is skipped because as we used space seperated by comma.
+console.log(b, c, d); // apple, chickoo, muskmelon
